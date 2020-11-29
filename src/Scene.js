e365e08000636797d8d7c2f9bd696765e6e79149
asjadanis <asjadanis128@gmail.com>
2020-11-30 04:50:59 +0500

    formatted code

--- src/Scene.js
+++ src/Scene.js
@@ -1,12 +1,11 @@
-import React, {Component} from 'react';
-import * as THREE from 'three';
-import OrbitControls from 'three-orbitcontrols';
+import React, { Component } from "react";
+import * as THREE from "three";
+import OrbitControls from "three-orbitcontrols";
 
-class Scene extends Component{
-  constructor(props){
+class Scene extends Component {
+  constructor(props) {
     super(props);
-    this.state={
-    }
+    this.state = {};
     this.start = this.start.bind(this);
     this.stop = this.stop.bind(this);
     this.animate = this.animate.bind(this);
@@ -17,54 +16,59 @@ class Scene extends Component{
     this.handleWindowResize = this.handleWindowResize.bind(this);
   }
 
-  componentWillMount(){
-    window.addEventListener('resize', this.handleWindowResize)
+  componentWillMount() {
+    window.addEventListener("resize", this.handleWindowResize);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.setupScene();
   }
 
-  setupScene(){
+  setupScene() {
     this.width = this.container.clientWidth;
     this.height = this.container.clientHeight;
-    
-    const renderer = new THREE.WebGLRenderer({antialias: true });
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.shadowMap.enabled = true;
     renderer.gammaOutput = true;
-    renderer.gammaFactor = 2.2
+    renderer.gammaFactor = 2.2;
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
     let scene = new THREE.Scene();
-    scene.background = new THREE.Color('black');
+    scene.background = new THREE.Color("black");
 
-    let camera = new THREE.PerspectiveCamera(60, this.width/this.height, 0.25, 1000);
+    let camera = new THREE.PerspectiveCamera(
+      60,
+      this.width / this.height,
+      0.25,
+      1000
+    );
     scene.add(camera);
 
     let sphere = new THREE.SphereGeometry(50, 300, 300);
     let material = new THREE.MeshPhongMaterial({
-      map: new THREE.TextureLoader().load('/Assets/2_no_clouds_4k.jpg'),
-      bumpMap: new THREE.TextureLoader().load('/Assets/elev_bump_4k.jpg'),  
+      map: new THREE.TextureLoader().load("/Assets/2_no_clouds_4k.jpg"),
+      bumpMap: new THREE.TextureLoader().load("/Assets/elev_bump_4k.jpg"),
       bumpScale: 0.005,
-      specularMap: THREE.ImageUtils.loadTexture('/Assets/water_4k.png'),
-      specular: new THREE.Color('grey'),
-    }); 
+      specularMap: THREE.ImageUtils.loadTexture("/Assets/water_4k.png"),
+      specular: new THREE.Color("grey"),
+    });
     let mesh = new THREE.Mesh(sphere, material);
     scene.add(mesh);
-    sphere = new THREE.SphereGeometry(50.1, 300, 300)
+    sphere = new THREE.SphereGeometry(50.1, 300, 300);
     material = new THREE.MeshPhongMaterial({
-      map: new THREE.TextureLoader().load('/Assets/fair_clouds_4k.png'),
-      transparent: true
-    })
+      map: new THREE.TextureLoader().load("/Assets/fair_clouds_4k.png"),
+      transparent: true,
+    });
     mesh = new THREE.Mesh(sphere, material);
-    scene.add(mesh)
+    scene.add(mesh);
     this.renderer = renderer;
     this.scene = scene;
     this.camera = camera;
     this.object = mesh;
 
-    let spotLight = new THREE.SpotLight(0xffffff, 0.25)
+    let spotLight = new THREE.SpotLight(0xffffff, 0.25);
     spotLight.position.set(45, 50, 15);
     camera.add(spotLight);
     this.spotLight = spotLight;
@@ -76,25 +80,25 @@ class Scene extends Component{
     this.computeBoundingBox();
   }
 
-  computeBoundingBox(){
-    let offset = 1.60;
+  computeBoundingBox() {
+    let offset = 1.6;
     const boundingBox = new THREE.Box3();
     boundingBox.setFromObject(this.object);
     const center = boundingBox.getCenter();
     const size = boundingBox.getSize();
-    const maxDim = Math.max( size.x, size.y, size.z );
-    const fov = this.camera.fov * ( Math.PI / 180 );
-    let cameraZ = maxDim / 2 / Math.tan( fov / 2 );
+    const maxDim = Math.max(size.x, size.y, size.z);
+    const fov = this.camera.fov * (Math.PI / 180);
+    let cameraZ = maxDim / 2 / Math.tan(fov / 2);
     cameraZ *= offset;
     this.camera.position.z = center.z + cameraZ;
     const minZ = boundingBox.min.z;
-    const cameraToFarEdge = ( minZ < 0 ) ? -minZ + cameraZ : cameraZ - minZ;
+    const cameraToFarEdge = minZ < 0 ? -minZ + cameraZ : cameraZ - minZ;
 
     this.camera.far = cameraToFarEdge * 3;
     this.camera.lookAt(center);
     this.camera.updateProjectionMatrix();
 
-    let controls = new OrbitControls( this.camera, this.renderer.domElement );
+    let controls = new OrbitControls(this.camera, this.renderer.domElement);
     controls.enableDamping = true;
     controls.dampingFactor = 0.25;
     controls.enableZoom = true;
@@ -113,15 +117,15 @@ class Scene extends Component{
     this.container.appendChild(this.renderer.domElement);
     this.start();
   }
-  
-  start(){
+
+  start() {
     if (!this.frameId) {
-      this.frameId = requestAnimationFrame(this.animate)
+      this.frameId = requestAnimationFrame(this.animate);
     }
   }
 
-  renderScene(){
-    this.renderer.render(this.scene, this.camera)
+  renderScene() {
+    this.renderer.render(this.scene, this.camera);
   }
 
   animate() {
@@ -134,19 +138,19 @@ class Scene extends Component{
     cancelAnimationFrame(this.frameId);
   }
 
-  handleWindowResize(){
+  handleWindowResize() {
     let width = window.innerWidth;
     let height = window.innerHeight;
-    this.camera.aspect = width/height;
+    this.camera.aspect = width / height;
     this.camera.updateProjectionMatrix();
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.stop();
     this.destroyContext();
   }
 
-  destroyContext(){
+  destroyContext() {
     this.container.removeChild(this.renderer.domElement);
     this.renderer.forceContextLoss();
     this.renderer.context = null;
@@ -154,19 +158,22 @@ class Scene extends Component{
     this.renderer = null;
   }
 
-
-  render(){
-    const width = '100%';
-    const height = '100%';
-    return(
-      <div 
-        ref={(container) => {this.container = container}}
-        style={{width: width, height: height, position: 'absolute', overflow: 'hidden'}}
-      >
-      </div>
-    )
+  render() {
+    const width = "100%";
+    const height = "100%";
+    return (
+      <div
+        ref={(container) => {
+          this.container = container;
+        }}
+        style={{
+          width: width,
+          height: height,
+          position: "absolute",
+          overflow: "hidden",
+        }}></div>
+    );
   }
-  
 }
 
 export default Scene;
